1b2a86ff3164d9a7f59ca592a0c9eef6166e853e
shreyas bharadwaj <74963699+shreyas-bk24@users.noreply.github.com>
2022-10-24 21:50:30 +0530

    Add files via upload

---
+++ MediKit/static/js/payment.js
@@ -0,0 +1,4 @@
+const name = document.getElementById("name")
+const number = document.getElementById("number")
+const frame = document.getElementById("frame")
+
--- MediKit/static/js/validate.js
+++ MediKit/static/js/validate.js
@@ -1,1 +1,32 @@
+var first_name=document.getElementById('firstname').value;
+var last_name=document.getElementById('lastname').value;
+var email=document.getElementById('email').value;
+var password1=document.getElementById('password').value;
+var password2=document.getElementById('password2').value;
+
+// validations for first name
+var firstname_val=()=>{
+if((first_name.length)<2){
+    document.getElementById('fname_message').innerHTML="Enter a Valid first name";
+    
+}   
+}
+
+// validations for lastname
+var lastname_val=()=>{
+    if(last_name=="" | last_name==none){
+        document.getElementById('lname_err').innerHTML="last name is required";
+    }
+}
+
+// validations for email
+
+var validate_email=(email)=>{
+    if(/^\w+([\.-]?\w)*@\w+)*(\.\w{2,3})+$/.test(email)){
+        return true;
+    }
+    else{
+        document.getElementById('email_err').innerHTML('Enter a valid email id')
+    }
+}
 
