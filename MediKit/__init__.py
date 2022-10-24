4cf29f4525d95e4cfb447dda2748e479264abd73
shreyas bharadwaj <74963699+shreyas-bk24@users.noreply.github.com>
2022-10-24 21:41:02 +0530

    Update __init__.py

--- MediKit/__init__.py
+++ MediKit/__init__.py
@@ -1,1 +1,9 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///medi-kit.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+from MediKit import routes, Vendor, UserLogin, cart, category, forgot_password
